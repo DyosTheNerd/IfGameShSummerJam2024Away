43ae553cbc71de78f2e7fa690771ba202845dcfa
DyosTheNerd <irondwarf@gmx.de>
2024-07-14 15:14:52 +0200

    simple sound effects

---
+++ Assets/sfx/SFX.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX : MonoBehaviour
+{
+    public string name; 
+    
+    private AudioSource audioSource;
+    
+    public bool multiple = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundEffectsMaster.instance.registerSoundEffect(this);   
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    
+    public void playSoundEffect()
+    {
+        if (audioSource.isPlaying && !multiple)
+        {
+            return;
+        }
+        audioSource.Play();
+    }
+}
